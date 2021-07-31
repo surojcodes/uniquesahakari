c8cec306df5c6eb72c63e05395546a63b9ce9351
Suroj Maharjan <surojcodes@gmail.com>
2021-07-31 11:34:49 +0545

    spacing

--- resources/views/pages/about/introduction.blade.php
+++ resources/views/pages/about/introduction.blade.php
@@ -3,17 +3,17 @@
 Unique Cooperative | Introduction
 @endsection
 @section('content')
-<div class="container my-3">
- <div class="section-title">
-    <span>Introduction</span>
-    <h2>Introduction</h2>
-    <hr style='width:40%'>
-    </div>
-      @if($introduction->image)
-      <div class="text-center">
-        <img src="/storage/about/{{$introduction->image}}" alt="Introduction" width="900px">
-      </div>
-      @endif
-      {!!$introduction->text!!}
+<div class="container my-3 px-5">
+  <div class="section-title">
+  <span>Introduction</span>
+  <h2>Introduction</h2>
+  <hr style='width:40%'>
+  </div>
+  @if($introduction->image)
+  <div class="text-center">
+    <img src="/storage/about/{{$introduction->image}}" alt="Introduction" width="900px">
+  </div>
+  @endif
+  {!!$introduction->text!!}
 </div>
 @endsection
