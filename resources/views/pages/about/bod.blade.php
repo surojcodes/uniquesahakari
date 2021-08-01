0b86485767dfceb76c84310ece5339ea70c66200
Suroj Maharjan <surojcodes@gmail.com>
2021-08-01 10:08:36 +0545

    BOD card style change

--- resources/views/pages/about/bod.blade.php
+++ resources/views/pages/about/bod.blade.php
@@ -3,29 +3,29 @@
 Unique Cooperative | Board of Directors
 @endsection
 @section('content')
-<div class="container my-3">
- <div class="section-title">
-    <span>Board of Directors</span>
-    <h2>Board of Directors</h2>
-    <hr style='width:40%'>
-    <div class="row justify-content-center align-items-center">
+ <section id="team" class="team">
+      <div class="container">
+        <div class="section-title">
+          <span>Board of Directors</span>
+          <h2>Board of Directors</h2>
+        </div>
+        <div class="row">
       @forelse($members as $member)
-      <div class="col-lg-4 col-md-6 col-sm-6">
-        <div class="card shadow-sm mb-4">
-          <img src="/storage/bod/{{$member->image}}" class="card-img-top" alt="{{$member->name}}">
-          <div class="card-body">
-            <div class="text-center">
-              <h4><strong>{{$member->name}}</strong></h4>
-              <h5> <em>{{$member->position}}</em></h5>
-            <p class="card-text">{{$member->text}}</p>
+          <div class="col-lg-4 col-md-6 col-sm-6 text-center">
+            <div class="member d-flex align-items-center flex-column ">
+              <div class="pic"><img src="/storage/bod/{{$member->image}}" class="img-fluid" alt="{{$member->name}}"></div>
+              <div class="member-info mt-3">
+                <h4>{{$member->name}}</h4>
+                <span class="pb-0">{{$member->position}}</span>
+                <hr>
+                <p>{{$member->text}}</p>
+              </div>
             </div>
           </div>
-        </div>
-      </div>
       @empty
         <h4 class="text-center">No members!</h4>
       @endforelse
-    </div>
-  </div>
-</div>
+        </div>
+      </div>
+    </section>
 @endsection
